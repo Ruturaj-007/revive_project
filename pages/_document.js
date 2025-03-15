59ee0d64efa6462622554760e8d3a75414e84f2f
Ruturaj-007 <pruturaj3003@gmail.com>
2025-03-15 20:13:27 +0530

    added

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Geist&display=swap"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
